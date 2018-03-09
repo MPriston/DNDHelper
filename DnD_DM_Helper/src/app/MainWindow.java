d7e76338a2d043d2df411366e494cd8ef8b475e0
Matt <matheuscarruthers@gmail.com>
2018-03-08 17:30:09 -0800

    Add some fields to FormMPanel

--- DnD_DM_Helper/src/app/MainWindow.java
+++ DnD_DM_Helper/src/app/MainWindow.java
@@ -10,6 +10,10 @@ public class MainWindow {
 	private JFrame frmDmHelper;
 	private JPanel iniPanel, formMPanel;
 	private JLabel quote;
+	private JTextField tfMonst;
+	private JTextField tfHpF;
+	private JTextField tfHpB;
+	private JTextField tfSpeed;
 	
 	
 	private void falseall() {
@@ -77,6 +81,141 @@ public class MainWindow {
 		formMPanel.setBackground(new Color(255, 228, 181));
 		frmDmHelper.getContentPane().add(formMPanel, "name_954097600786134");
 		formMPanel.setLayout(null);
+		
+		JLabel lblMonster = new JLabel("Monster:");
+		lblMonster.setForeground(new Color(128, 0, 0));
+		lblMonster.setFont(new Font("Palatino Linotype", Font.BOLD, 24));
+		lblMonster.setHorizontalAlignment(SwingConstants.LEFT);
+		lblMonster.setBounds(12, 13, 98, 24);
+		formMPanel.add(lblMonster);
+		
+		tfMonst = new JTextField();
+		lblMonster.setLabelFor(tfMonst);
+		tfMonst.setFont(new Font("Palatino Linotype", Font.PLAIN, 20));
+		tfMonst.setBounds(115, 13, 149, 24);
+		formMPanel.add(tfMonst);
+		tfMonst.setColumns(10);
+		
+		JLabel lblSize = new JLabel("Size:");
+		lblSize.setForeground(new Color(128, 0, 0));
+		lblSize.setFont(new Font("Palatino Linotype", Font.BOLD, 24));
+		lblSize.setBounds(12, 50, 56, 24);
+		formMPanel.add(lblSize);
+		
+		JComboBox cbSize = new JComboBox();
+		cbSize.setFont(new Font("Palatino Linotype", Font.PLAIN, 20));
+		cbSize.setModel(new DefaultComboBoxModel(new String[] {"Tiny", "Small", "Medium", "Large", "Huge", "Gargantuan"}));
+		cbSize.setBounds(69, 50, 184, 25);
+		formMPanel.add(cbSize);
+		
+		JLabel lblType = new JLabel("Type:");
+		lblType.setForeground(new Color(128, 0, 0));
+		lblType.setFont(new Font("Palatino Linotype", Font.BOLD, 24));
+		lblType.setBounds(12, 86, 69, 24);
+		formMPanel.add(lblType);
+		
+		JComboBox cbType = new JComboBox();
+		cbType.setModel(new DefaultComboBoxModel(new String[] {"Aberration", "Beast", "Celestial", "Construct", "Dragon", "Elemental", "Fey", "Fiend", "Giant", "Humanoid", "Monstrosity", "Ooze", "Plant", "Undead"}));
+		cbType.setFont(new Font("Palatino Linotype", Font.PLAIN, 20));
+		cbType.setBounds(80, 87, 184, 24);
+		formMPanel.add(cbType);
+		
+		JLabel lblAl = new JLabel("Alignment:");
+		lblAl.setForeground(new Color(128, 0, 0));
+		lblAl.setFont(new Font("Palatino Linotype", Font.BOLD, 24));
+		lblAl.setBackground(Color.WHITE);
+		lblAl.setBounds(12, 123, 129, 25);
+		formMPanel.add(lblAl);
+		
+		JComboBox cbAlig = new JComboBox();
+		cbAlig.setForeground(new Color(0, 0, 0));
+		cbAlig.setFont(new Font("Palatino Linotype", Font.PLAIN, 20));
+		cbAlig.setModel(new DefaultComboBoxModel(new String[] {"Lawful Good", "Neutral Good", "Chaotic Good", "Lawful Neutral", "Neutral", "Chaotic Neutral", "Lawful Evil", "Neutral Evil", "Chotic Evil", "Unaligned"}));
+		cbAlig.setBounds(139, 125, 184, 25);
+		formMPanel.add(cbAlig);
+		
+		JLabel lblArmorClass = new JLabel("Armor Class:");
+		lblArmorClass.setForeground(new Color(128, 0, 0));
+		lblArmorClass.setFont(new Font("Palatino Linotype", Font.BOLD, 24));
+		lblArmorClass.setBounds(12, 161, 141, 24);
+		formMPanel.add(lblArmorClass);
+		
+		JSpinner sAC = new JSpinner();
+		sAC.setModel(new SpinnerNumberModel(new Integer(0), null, null, new Integer(1)));
+		sAC.setFont(new Font("Palatino Linotype", Font.PLAIN, 20));
+		sAC.setBounds(160, 162, 45, 24);
+		formMPanel.add(sAC);
+		
+		JLabel lblHp = new JLabel("Hp:");
+		lblHp.setForeground(new Color(128, 0, 0));
+		lblHp.setFont(new Font("Palatino Linotype", Font.BOLD, 24));
+		lblHp.setBounds(12, 198, 45, 24);
+		formMPanel.add(lblHp);
+		
+		JSpinner sHP = new JSpinner();
+		sHP.setFont(new Font("Palatino Linotype", Font.PLAIN, 20));
+		sHP.setBounds(56, 199, 56, 24);
+		formMPanel.add(sHP);
+		
+		JLabel lblEx = new JLabel("Ex: 122");
+		lblEx.setFont(new Font("Palatino Linotype", Font.PLAIN, 15));
+		lblEx.setForeground(new Color(128, 0, 0));
+		lblEx.setBounds(124, 204, 56, 16);
+		formMPanel.add(lblEx);
+		
+		JLabel lblHpFormula = new JLabel("Hp Formula:");
+		lblHpFormula.setForeground(new Color(128, 0, 0));
+		lblHpFormula.setFont(new Font("Palatino Linotype", Font.BOLD, 24));
+		lblHpFormula.setBounds(12, 235, 141, 24);
+		formMPanel.add(lblHpFormula);
+		
+		tfHpF = new JTextField();
+		tfHpF.setFont(new Font("Palatino Linotype", Font.PLAIN, 20));
+		tfHpF.setBounds(151, 236, 69, 24);
+		formMPanel.add(tfHpF);
+		tfHpF.setColumns(10);
+		
+		JLabel lblex2 = new JLabel("Ex: 12d8");
+		lblex2.setForeground(new Color(128, 0, 0));
+		lblex2.setFont(new Font("Palatino Linotype", Font.PLAIN, 15));
+		lblex2.setBounds(230, 241, 69, 16);
+		formMPanel.add(lblex2);
+		
+		JLabel lblHpBonus = new JLabel("Hp Bonus:");
+		lblHpBonus.setForeground(new Color(128, 0, 0));
+		lblHpBonus.setFont(new Font("Palatino Linotype", Font.BOLD, 24));
+		lblHpBonus.setBounds(12, 272, 116, 24);
+		formMPanel.add(lblHpBonus);
+		
+		tfHpB = new JTextField();
+		tfHpB.setFont(new Font("Palatino Linotype", Font.PLAIN, 20));
+		tfHpB.setBounds(133, 273, 63, 24);
+		formMPanel.add(tfHpB);
+		tfHpB.setColumns(10);
+		
+		JLabel lblExOr = new JLabel("Ex: +36 or -34");
+		lblExOr.setForeground(new Color(128, 0, 0));
+		lblExOr.setFont(new Font("Palatino Linotype", Font.PLAIN, 15));
+		lblExOr.setBounds(208, 274, 98, 24);
+		formMPanel.add(lblExOr);
+		
+		JLabel lblSpeed = new JLabel("Speed:");
+		lblSpeed.setForeground(new Color(128, 0, 0));
+		lblSpeed.setFont(new Font("Palatino Linotype", Font.BOLD, 24));
+		lblSpeed.setBounds(12, 309, 79, 24);
+		formMPanel.add(lblSpeed);
+		
+		tfSpeed = new JTextField();
+		tfSpeed.setFont(new Font("Palatino Linotype", Font.PLAIN, 20));
+		tfSpeed.setBounds(90, 310, 174, 24);
+		formMPanel.add(tfSpeed);
+		tfSpeed.setColumns(10);
+		
+		JLabel lblStr = new JLabel("Strength:");
+		lblStr.setForeground(new Color(128, 0, 0));
+		lblStr.setFont(new Font("Palatino Linotype", Font.BOLD, 24));
+		lblStr.setBounds(12, 346, 101, 25);
+		formMPanel.add(lblStr);
 		
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setBackground(new Color(255, 228, 181));
